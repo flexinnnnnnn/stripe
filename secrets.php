eded57211df363df86a0ed8b964202af7cf8d3e3
flexinnnnnnn <166776566+flexinnnnnnn@users.noreply.github.com>
2024-04-11 22:57:48 -0300

    Add files via upload

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51P4Vqw00McvKHYLKhLQ6wh24f8nSxbj2PaHHKmS48l6W4N6zgQY3AJOEBrQvv3SSQQo4wAr2Lyc6TMBgo4LnPM7500qHnbuZBM';
